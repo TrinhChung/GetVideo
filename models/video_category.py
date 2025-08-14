44324628c3cd99c79ba86ea1d46176625abe2914
Trinh Văn Chung <75870812+TrinhChung@users.noreply.github.com>
2025-08-14 15:08:37 +0900

    Remove unused category and history models

--- models/video_category.py
+++
@@ -1,9 +0,0 @@
-# models/video_category.py
-from database_init import db
-
-
-class VideoCategory(db.Model):
-    __tablename__ = "video_category"
-
-    video_id = db.Column(db.Integer, db.ForeignKey("video.id"), primary_key=True)
-    category_id = db.Column(db.Integer, db.ForeignKey("category.id"), primary_key=True)
